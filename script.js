3e3489b15b8db3111e60c592b70a4f8453d2a58b
Tamim Slimi <38198296-tamim29012@users.noreply.replit.com>
2025-01-27 01:42:05 +0000

    sdvwvgds

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
-const submitText = function() {
-  const inputValue = document.querySelector("#myInput").value;
-  document.querySelector('#box').innerHTML += `<div class="inside">${inputValue}</div>`;
-  document.querySelector("#myInput").value = '';
-}
+let todos = [];
 
-const keyboardInput = function(event){
-  if(event.keyCode == 13) {
-    submitText();
+function makeHTML(){
+      let html = '';
+      todos.forEach(todo => {
+          const divClasses = todo.done ? 'todo done' : 'todo';
+          html += `
+          <div class="${divClasses}" onclick="todoDone(${todo.id})">
+              <span>
+                ${todo.text}
+              </span>
+              <span 
+                class="material-icons"
+                onclick="deleteTodo(${todo.id})"
+              >
+                &#xe92b;
+              </span>
+          </div>
+          `;
+      });
+
+      document.querySelector('#todos').innerHTML = html
+  }
+
+makeHTML()
+function addTodo(){
+  const text = document.querySelector("#newTodo").value
+  if(text.length > 0){
+    let id = 1;
+    todos.forEach(todo => {
+      id = Math.max(todo.id, id);
+    });
+    id++;
+    todos.push({id: id, text: text, done:false});
+    document.querySelector("#newTodo").value = '';
+    makeHTML();
   }
 }
-const clearBox = function() {
-  document.querySelector('#box').innerHTML = '';
+function deleteTodo(id){
+    todos = todos.filter(t => t.id != id);
+    makeHTML();
 }
-const deleteLast = function(deleteLast)
+
+function todoDone(id) {
+  const todo = todos.find(t => t.id == id);
+  todo.done = !todo.done;
+  makeHTML();
+}
